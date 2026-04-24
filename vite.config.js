c6d6efa56cf7ea43fdbe21a9cd5a14747f8559bc
Wayne Laker <waynelaker@gmail.com>
2026-04-24 17:43:00 +1200

    Fix vite config: add __dirname for ES modules compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import { glob } from 'glob'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/',
